4e9b7f251ce9cd1b14b3d6c170c43d08e5ee221c
igiannakas <ioannis@my-spot.co.uk>
2023-12-21 10:30:47 +0000

    Introduce dialog box to disable ensure vertical shell thickness

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -1532,7 +1532,7 @@ void PrintObject::discover_vertical_shells()
         bool has_extra_layers = false;
         for (size_t region_id = 0; region_id < this->num_printing_regions(); ++region_id) {
             const PrintRegionConfig &config = this->printing_region(region_id).config();
-            if (config.ensure_vertical_shell_thickness.value && !config.alternate_extra_wall) {
+            if (config.ensure_vertical_shell_thickness.value) {
                 has_extra_layers = true;
                 break;
             }
@@ -1610,7 +1610,7 @@ void PrintObject::discover_vertical_shells()
 
     for (size_t region_id = 0; region_id < this->num_printing_regions(); ++ region_id) {
         const PrintRegion &region = this->printing_region(region_id);
-        if (! region.config().ensure_vertical_shell_thickness.value || region.config().alternate_extra_wall.value)
+        if (! region.config().ensure_vertical_shell_thickness.value)
             // This region will be handled by discover_horizontal_shells().
             continue;
 
@@ -3136,7 +3136,7 @@ void PrintObject::discover_horizontal_shells()
 #endif
 
             // If ensure_vertical_shell_thickness, then the rest has already been performed by discover_vertical_shells().
-            if (region_config.ensure_vertical_shell_thickness.value && !region_config.alternate_extra_wall.value)
+            if (region_config.ensure_vertical_shell_thickness.value)
                 continue;
 
             coordf_t print_z  = layer->print_z;
